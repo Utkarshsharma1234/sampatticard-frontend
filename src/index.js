82b29e37cb98a6641b8408ac14b5623683b10865
Utkarshsharma1234 <96917045+Utkarshsharma1234@users.noreply.github.com>
2024-01-24 02:26:41 +0530

    frontend

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-<<<<<<< HEAD
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-=======
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App'
->>>>>>> b508e5529526d7596089eacbdaaf334581addc91
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
